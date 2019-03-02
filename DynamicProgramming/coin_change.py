71b4a5d042a22b614499e9e5193e660d5762296b
KalyanTarun <kalyantarun99@gmail.com>
2019-03-02 12:32:05 +0530

    Dynamic approach

--- DynamicProgramming/coin_change.py
+++ DynamicProgramming/coin_change.py
@@ -4,8 +4,9 @@ Created on Sat Mar  2 11:38:02 2019
 
 @author: Tarun
 """
+#Coin change applicable when coins are sorted
 from copy import deepcopy
-coins=[1,2,3]
+coins=sorted([1,2,3])
 n=4
 solns=0
 
@@ -13,12 +14,20 @@ def coins_change(bag,coin):
       global coins
       global n
       global solns
-      bag.append(coin)
+      
+      if coin is not None:
+          bag.append(coin)
       
       if sum(bag) == n:
           solns+=1
-      if sum(bag) < n:
+          print(bag)
+          
+      if sum(bag) < n or coin is None:
           for coin_value in coins:
+              #Dp approach to eliminate the path if sum>n and if coin being added is greater than
+              #last element in bag i.e to avoid duplicates
+              if sum(bag) + coin_value > n or ( len(bag) > 0 and bag[-1] < coin_value):
+                  break
               coins_change(deepcopy(bag),coin_value)
               
       
@@ -27,5 +36,7 @@ def coins_change(bag,coin):
     
 def main():
     global solns
-    coins_change([],coins[0])
+    coins_change([], None)
     print(solns)
+    
+main()
